e7d6e9acdc06f54d37d2615dfa79770b83f75678
anvy1102 <langocthuyan@gmail.com>
2020-05-16 00:38:27 +0700

    clean project

--- lib/userhistory_lib/old/path_config_hidden.py
+++
@@ -1,6 +0,0 @@
-METARAW_FOLDERDIR = '/data/BIGDATA/fplay/recommend/meta/raws'
-VIDEOS_FILENAME = 'videos.json'
-VIDEOTYPES_FILENAME = 'videos_types.json'
-
-USERHIS_FOLDERDIR = '/data/BIGDATA/fplay/recommend/trending/stats'
-USERHIS_FILENAME = 'user_items.json'
