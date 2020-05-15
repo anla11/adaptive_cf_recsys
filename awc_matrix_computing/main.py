82c8f117ebbeaa953811fc24270b73937f33dd95
anvy1102 <langocthuyan@gmail.com>
2020-05-15 21:35:55 +0700

    fix errors

--- awc_matrix_computing/main.py
+++ awc_matrix_computing/main.py
@@ -9,5 +9,5 @@ log_df = pd.read_csv(datapath)
 log_parser = LOG_PARSER(log_df)
 awc = AWC_REC(log_parser, parameters={'gamma':0.5, 'lambda':0.7})
 item_his = log_parser.get_itemids()[0]
-rec_df = awc.recommend(item_his)
+rec_df = awc.recommend([item_his])
 print (rec_df)
